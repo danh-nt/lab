f10bf51996f8b97add977fb9d658b1f5d44e71a8
danh-nt <125142607+danh-nt@users.noreply.github.com>
2023-04-14 21:02:08 +0700

    Create BaiTap.java

---
+++ DoAn/source_code/BaiTap.java
@@ -0,0 +1,35 @@
+package quanlisgk;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class BaiTap extends SGK {
+
+    public BaiTap() {
+    }
+
+    public BaiTap(String maSach, String tenSach, String nhaXuatBan, int lop, int namXuatBan, int soLuong, double giaSach, Date ngayNhap) {
+        super(maSach, tenSach, lop, namXuatBan, nhaXuatBan, soLuong, giaSach, ngayNhap);
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String ngayNhapLT = sdf.format(ngayNhap);
+        return "Sach Bai tap [" + "Ma sach: " + maSach + ", ten sach: " + tenSach + ", lop: " + lop + ", nam xuat ban: " + namXuatBan + ", nha xuat ban: " + nhaXuatBan + ", so luong: " + soLuong + ", gia sach: " + giaSach + ", ngay nhap: " + ngayNhapLT + "]";
+    }
+    
+    @Override
+        public Date hien(int day,int month,int year){
+        Calendar cl = Calendar.getInstance();
+        cl.set(year, month-1, day);
+        return this.ngayNhap = cl.getTime();
+    }
+    @Override
+        public Date xuat(int day,int month,int year){
+        Calendar cl = Calendar.getInstance();
+        cl.set(year, month-1, day);
+        return this.ngayXuat=cl.getTime();
+    }
+}
